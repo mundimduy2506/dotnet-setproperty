7dd6e110df275b521485627bb30d084198e3fe8e
Duy Tran <duytq2506@gmail.com>
2017-11-09 13:58:03 -0500

    added dotnet setproperty project

--- dotnet-setproperty/Program.cs
+++ dotnet-setproperty/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace dotnet_setproperty
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("Missing version string");
+                    return 1;
+                }
+
+                var versionString = args[0];
+                var currentDirectory = Directory.GetCurrentDirectory();
+                var csprojFile = Directory.EnumerateFileSystemEntries(currentDirectory, "*.csproj", SearchOption.TopDirectoryOnly).First();
+                var document = XDocument.Load(csprojFile);
+
+                document.GetOrCreateElement("Project")
+                    .GetOrCreateElement("PropertyGroup")
+                    .GetOrCreateElement("Version")
+                    .SetValue(versionString);
+
+                File.WriteAllText(csprojFile, document.ToString());
+                Console.WriteLine($"Setting version: {versionString}");
+                return 0;
+            }
+        }
+    }
+    public static class Extensions
+    {
+        /// <remarks>
+        /// Source: http://stackoverflow.com/a/14892813/1636276
+        /// </remarks>
+        public static XElement GetOrCreateElement(this XContainer container, string name)
+        {
+            var element = container.Element(name);
+            if (element == null)
+            {
+                element = new XElement(name);
+                container.Add(element);
+            }
+            return element;
         }
     }
 }
